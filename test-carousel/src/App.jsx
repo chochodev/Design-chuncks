2343e9346a32c292b0cd1308dc0a61504bebce0d
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-02-19 14:06:56 +0100

    adding test and image carousel

---
+++ test-carousel/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Slider from '.'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div>
+          <h2>Slider</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium laudantium similique sequi velit totam suscipit exercitationem sunt doloremque eius aut voluptatem obcaecati modi optio recusandae quam impedit, voluptate illo praesentium officiis? Excepturi, eius? Ipsum quidem voluptatum incidunt sapiente quos expedita consequatur, suscipit quibusdam perspiciatis quaerat magni possimus enim asperiores alias! Rem dolore nesciunt quisquam praesentium delectus laborum quae culpa nam harum? Deleniti ab aperiam ad, adipisci quidem, nihil possimus error sit aspernatur omnis reprehenderit voluptatem aliquam nemo quasi, asperiores accusamus magnam repellat inventore dolores sed non quod quibusdam qui sunt! Quisquam ducimus neque voluptatibus, inventore cupiditate hic quae culpa libero debitis quod ipsum cumque at dignissimos nemo odit aliquam nihil sapiente fugiat unde atque? Mollitia quo at reprehenderit ipsa! Suscipit earum nobis vel velit tempora sequi, assumenda sint illo, consequuntur aut, sapiente aperiam nihil ad provident cupiditate? Consequuntur atque laborum neque veritatis eos? Enim suscipit veritatis reiciendis a omnis ex iste ab nihil. Sunt excepturi consequuntur repellendus omnis. Consectetur saepe repudiandae ex. Consequuntur amet sint recusandae consequatur, laudantium temporibus ipsa reiciendis, ad, nobis velit exercitationem quasi! Maxime soluta quo illum pariatur vero velit, impedit fugiat non natus minus aperiam sequi in iure enim inventore possimus dicta aspernatur. Quibusdam, quam sunt?</p>
+        </div>
+        <div className='slider_component'>
+          <Slider />
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
